29b0c2b86e405aacc29e3b26a24279e7bde1d8b3
Daniel Villera <davillera77@gmail.com>
2024-09-02 14:31:52 -0500

    docs and asscoeiations

--- src/models/associations.js
+++ src/models/associations.js
@@ -1,5 +1,6 @@
 const User = require('./user');
 const Task = require('./task');
+const Comment = require('./comment');
 
 // Un usuario tiene muchas tareas
 User.hasMany(Task, {
@@ -17,7 +18,21 @@ Task.belongsTo(User, {
   onDelete: 'CASCADE',
 });
 
+
+Task.hasMany(Comment, {
+  foreignKey: 'taskId',
+  onDelete: 'CASCADE', // Asegura que los comentarios se eliminen si la tarea se elimina
+  onUpdate: 'CASCADE',
+});
+
+Comment.belongsTo(Task, {
+  foreignKey: 'taskId',
+  onDelete: 'CASCADE',
+  onUpdate: 'CASCADE',
+});
+
 module.exports = {
   User,
   Task,
+  Comment
 };
